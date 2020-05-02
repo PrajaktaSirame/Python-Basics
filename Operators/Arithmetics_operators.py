f900d78966d8ce0f731d11e06e3a00db06fc7002
Prajakta Sirame <prajaktasirame@gmail.com>
2020-05-02 18:06:18 +0530

    basics

---
+++ Operators/Arithmetics_operators.py
@@ -0,0 +1,9 @@
+a,b=10,5
+
+print('addition : ',a+b)
+print('subtraction : ',a-b)
+print('division : ',a/b)
+print('mul : ',a*b)
+print('Mod : ',a%b)
+print('pow : ',a**b)
+print('floor div : ',a//b)
---
+++ Operators/logical_operators.py
@@ -0,0 +1,9 @@
+x=20
+y=30
+
+
+print((x==25 and y==30))# for and both equation should be true 
+
+print((x==25 or y==30))# for or any one equation should be true
+
+print(not(x==25 or y==31))#not operator gives opposite ans
